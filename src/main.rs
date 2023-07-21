2f8914642ee3ec63b8371f5a0ebfbfb431c0aa1b
Uhtomsky <r.aizpurietis@gmail.com>
2023-07-21 16:08:36 +0300

    Initial_commint

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use pathfinding::prelude::bfs;
+
+#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+struct Pos(i32, i32);
+
+impl Pos {
+  fn successors(&self) -> Vec<Pos> {
+    let &Pos(x, y) = self;
+    vec![Pos(x+1,y+2), Pos(x+1,y-2), Pos(x-1,y+2), Pos(x-1,y-2),
+         Pos(x+2,y+1), Pos(x+2,y-1), Pos(x-2,y+1), Pos(x-2,y-1)]
+  }
+}
+
+static GOAL: Pos = Pos(4, 6);
+
+fn main() {
+let result = bfs(&Pos(1, 1), |p| p.successors(), |p| *p == GOAL);
+assert_eq!(result.expect("no path found").len(), 5);
+}
